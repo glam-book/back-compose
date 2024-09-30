0c019a8863785347419044bc12fc007fc2846db8
owpk <53643556+owpk@users.noreply.github.com>
2024-10-01 00:56:18 +0800

    SWAGGER: - Update docker-compose.yml for swagger configuration and database connection. - Add OpenAPI definition and server URL in SwaggerConfig.java. - Set default `swagger.url` in application.yml.

--- src/main/java/com/tlback/config/SwaggerConfig.java
+++ src/main/java/com/tlback/config/SwaggerConfig.java
@@ -3,10 +3,15 @@ package com.tlback.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
+@OpenAPIDefinition(servers = {
+        @Server(url = "${swagger.url}${server.servlet.context-path}", description = "Default Server URL")
+})
 public class SwaggerConfig {
 
     @Bean
