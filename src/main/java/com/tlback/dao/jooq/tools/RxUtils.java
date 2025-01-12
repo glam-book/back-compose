c8b426ccb0f73dfcc01c478274b8ddf9ad7ecc9e
owpk <53643556+owpk@users.noreply.github.com>
2025-01-12 17:04:40 +0800

    6-TELEGRAM_AUTH: Remove unused jooq imports from RxUtils
    - Remove unnecessary Insert, InsertReturningStep, and InsertReturning imports.
    - Simplify dependencies by eliminating redundant imports.

--- src/main/java/com/tlback/dao/jooq/tools/RxUtils.java
+++ src/main/java/com/tlback/dao/jooq/tools/RxUtils.java
@@ -3,12 +3,8 @@ package com.tlback.dao.jooq.tools;
 import java.util.List;
 import java.util.function.Function;
 
-import org.jooq.Insert;
-import org.jooq.InsertReturningStep;
 import org.jooq.Record;
 import org.jooq.Select;
-import org.jooq.impl.QOM.InsertReturning;
-
 import reactor.core.publisher.Flux;
 
 public final class RxUtils {
