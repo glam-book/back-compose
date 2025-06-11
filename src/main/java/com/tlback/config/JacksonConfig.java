57d9cd847be5a77d67fdf8ebf41ad3b1f38f6a02
owpk <vzvz4@mail.ru>
2025-06-12 03:50:23 +0800

    TL2-CACHE: chore(setup): remove eclipse configuration and update build scripts
    - Removed Eclipse-specific preferences files from `.settings` directory
    - Updated `.gitignore` to ignore `.settings` directory
    - Added `com.diffplug.eclipse.apt` and `eclipse` plugins in `build.gradle`
    - Configured Eclipse APT settings in `build.gradle`
    - Created a new task `generateEclipseConfig` for generating Eclipse configuration

--- src/main/java/com/tlback/config/JacksonConfig.java
+++ src/main/java/com/tlback/config/JacksonConfig.java
@@ -1,13 +1,11 @@
 package com.tlback.config;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 
 @Configuration
@@ -15,6 +13,7 @@ public class JacksonConfig {
 
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jacksonCustomizer() {
+        System.out.println("asd");
         return jacksonObjectMapperBuilder -> jacksonObjectMapperBuilder
                 .featuresToDisable(
                         DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
