d55abf7ab450cb8cc564d8247d25c205f4e42525
owpk <vzvz4@mail.ru>
2025-06-30 05:34:03 +0800

    FEATURE/REFACTOR-MODULES: fix gradle build

--- tl-core/src/main/java/com/tlback/core/web/telegram/TelegramWebhookController.java
+++ tl-core/src/main/java/com/tlback/core/web/telegram/TelegramWebhookController.java
@@ -2,16 +2,18 @@ package com.tlback.core.web.telegram;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-import com.tlback.core.config.AppArgs;
+import com.tlback.tg.bot.AppArgs;
 import com.tlback.tg.bot.TgGlamBot;
 
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class TelegramWebhookController {
     private final TgGlamBot tgGlamBot;
--- tl-tg-bot/src/main/java/com/tlback/tg/bot/TgBotConfiguration.java
+++ tl-tg-bot/src/main/java/com/tlback/tg/bot/TgBotConfiguration.java
@@ -1,9 +1,12 @@
 package com.tlback.tg.bot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import lombok.extern.slf4j.Slf4j;
@@ -11,10 +14,36 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Configuration
 public class TgBotConfiguration {
-	private static final String WEBHOOK_URL = "https://tl.ow-pk.ru";
+
+	@Value("${GLAM_TG_BOT_TOKEN}")
+	private String botToken;
+
+	@Value("${app.baseUrl}")
+	private String appBaseUrl;
 
 	@Bean
-	TelegramBotsApi telegramBotsApi() throws TelegramApiException {
-		return new TelegramBotsApi(DefaultBotSession.class);
+	TgGlamBot bot() {
+		return new TgGlamBot(botToken, AppArgs.TG_WEB_HOOK_PATH);
+	}
+
+	@Bean
+	TgBotInitializer botInitializer(TgGlamBot bot) {
+		return new TgBotInitializer(bot, appBaseUrl + AppArgs.TG_WEB_HOOK_PATH);
+	}
+
+	public static class TgBotInitializer {
+		private final TgGlamBot bot;
+		private final String url;
+
+		public TgBotInitializer(TgGlamBot bot, String url) {
+			this.bot = bot;
+			this.url = url;
+		}
+
+		@EventListener(ContextRefreshedEvent.class)
+		public void init() throws Exception {
+			var botApis = new TelegramBotsApi(DefaultBotSession.class);
+			botApis.registerBot(bot, new SetWebhook(url));
+		}
 	}
 }
