4ce249763da92ea00f4827c2d6cd05049d5cfef9
owpk <vzvz4@mail.ru>
2025-08-22 16:29:02 +0800

    HOTFIX/TG_BOT_FIX: fix envs

--- scripts/deploy-db.sh
+++ scripts/deploy-db.sh
@@ -2,25 +2,25 @@
 
 CONTAINER="tantal-db"
 
-export DB_NAME="${1:-tl_back}"
-export DB_PORT="${2:-5599}"
-export DB_PASSWORD="${3:-postgres}"
-export DB_USER="${4:-postgres}"
-export DB_HOST="${5:-localhost}"
+export TL_DB_NAME="${1:-tl_back}"
+export TL_DB_PORT="${2:-5599}"
+export TL_DB_PASSWORD="${3:-postgres}"
+export TL_DB_USER="${4:-postgres}"
+export TL_DB_HOST="${5:-localhost}"
 
 echo "Deploying database:"
-echo "Db name: $DB_NAME"
-echo "Db port: $DB_PORT"
-echo "Db pass: $DB_PASSWORD"
-echo "Db user: $DB_USER"
+echo "Db name: $TL_DB_NAME"
+echo "Db port: $TL_DB_PORT"
+echo "Db pass: $TL_DB_PASSWORD"
+echo "Db user: $TL_DB_USER"
 
 docker stop $CONTAINER 2> /dev/null
 
 docker run --name $CONTAINER \
-    -p $DB_PORT:5432 \
-    -e POSTGRES_PASSWORD=$DB_PASSWORD \
-    -e POSTGRES_USER=$DB_USER \
-    -e POSTGRES_DB=$DB_NAME \
+    -p $TL_DB_PORT:5432 \
+    -e POSTGRES_PASSWORD=$TL_DB_PASSWORD \
+    -e POSTGRES_USER=$TL_DB_USER \
+    -e POSTGRES_DB=$TL_DB_NAME \
     -d --rm postgres:17
 
 ./gradlew update
--- tl-tg-bot/src/main/java/com/tlback/tg/bot/TgBotConfiguration.java
+++ tl-tg-bot/src/main/java/com/tlback/tg/bot/TgBotConfiguration.java
@@ -1,12 +1,7 @@
 package com.tlback.tg.bot;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,32 +15,4 @@ public class TgBotConfiguration {
 	@Value("${app.baseUrl}")
 	private String appBaseUrl;
 
-	@Bean
-	TgGlamBot bot() {
-		return new TgGlamBot(botToken, AppArgs.TG_WEB_HOOK_PATH);
-	}
-
-	@Bean
-	TgGlamLongPolingBot longPolingBot() {
-		return new TgGlamLongPolingBot(botToken);
-	}
-
-	@Bean
-	TgBotInitializer botInitializer(TgGlamLongPolingBot bot) {
-		return new TgBotInitializer(bot);
-	}
-
-	public static class TgBotInitializer {
-		private final TgGlamLongPolingBot bot;
-
-		public TgBotInitializer(TgGlamLongPolingBot bot) {
-			this.bot = bot;
-		}
-
-		@EventListener(ContextRefreshedEvent.class)
-		public void init() throws Exception {
-			var botApis = new TelegramBotsApi(DefaultBotSession.class);
-			botApis.registerBot(bot);
-		}
-	}
 }
