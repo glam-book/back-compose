42052ca0f4bf257d6816dc768211b9f03067b3ed
owpk <vzvz4@mail.ru>
2025-09-19 20:51:39 +0800

    FEATURE/REFACTOR: fix link record to service

--- src/main/java/com/tlback/core/dao/jooq/JooqRecordToServiceRepository.java
+++ src/main/java/com/tlback/core/dao/jooq/JooqRecordToServiceRepository.java
@@ -14,28 +14,30 @@ import reactor.core.publisher.Mono;
 @Service
 @RequiredArgsConstructor
 public class JooqRecordToServiceRepository {
-    public static final RecordToService RECORD_TO_SERVICE_TABLE = RecordToService.RECORD_TO_SERVICE;
-    private final DSLContext dsl;
+        public static final RecordToService recordToServiceTable = RecordToService.RECORD_TO_SERVICE;
+        private final DSLContext dsl;
 
-    public Mono<Integer> linkRecordToService(Long recordId, Long serviceId) {
-        var sql = dsl.insertInto(com.tlback.jooq.gen.tables.RecordToService.RECORD_TO_SERVICE)
-                .set(RECORD_TO_SERVICE_TABLE.RECORD_ID, recordId)
-                .set(RECORD_TO_SERVICE_TABLE.SERVICE_ID, serviceId)
-                .onConflictDoNothing();
-        return Mono.from(sql);
-    }
+        public Mono<Integer> linkRecordToService(Long recordId, Long serviceId) {
+                var sql = dsl.insertInto(com.tlback.jooq.gen.tables.RecordToService.RECORD_TO_SERVICE)
+                                .set(recordToServiceTable.RECORD_ID, recordId)
+                                .set(recordToServiceTable.SERVICE_ID, serviceId)
+                                .onConflictDoNothing();
+                return Mono.from(sql);
+        }
 
-    public Mono<Integer> linkRecordToService(Long recordId, List<Long> serviceIds) {
-        var rows = serviceIds.stream()
-                .map(serviceId -> DSL.row(recordId, serviceId))
-                .toList();
+        public Mono<Integer> linkRecordToService(Long recordId, List<Long> serviceIds) {
+                var delete = dsl.deleteFrom(recordToServiceTable)
+                                .where(recordToServiceTable.RECORD_ID.eq(recordId))
+                                .and(recordToServiceTable.SERVICE_ID.notIn(serviceIds));
 
-        var sql = dsl.insertInto(RECORD_TO_SERVICE_TABLE)
-                .columns(RECORD_TO_SERVICE_TABLE.RECORD_ID, RECORD_TO_SERVICE_TABLE.SERVICE_ID)
-                .valuesOfRows(rows)
-                .onConflictDoNothing();
+                var insert = dsl.insertInto(recordToServiceTable)
+                                .columns(recordToServiceTable.RECORD_ID, recordToServiceTable.SERVICE_ID)
+                                .valuesOfRows(serviceIds.stream()
+                                                .map(serviceId -> DSL.row(recordId, serviceId))
+                                                .toList())
+                                .onConflictDoNothing();
 
-        return Mono.from(sql) // здесь вернётся Integer — количество вставленных строк
-                .map(count -> count);
-    }
+                return Mono.from(delete)
+                                .then(Mono.from(insert));
+        }
 }
