e36a64f3c4a9d38a8060ce020c460578be7d64a1
owpk <vzvz4@mail.ru>
2025-10-23 18:33:46 +0800

    HOTFIX/TG_WEBHOOK_PERMISSIONS: fix tg permissions

--- src/main/java/com/tlback/core/config/AppArgs.java
+++ src/main/java/com/tlback/core/config/AppArgs.java
@@ -4,5 +4,5 @@ public final class AppArgs {
     private AppArgs() {
     }
 
-    public static final String TG_WEB_HOOK_PATH = "/tg-bot";
+    public static final String TG_WEB_HOOK_PATH = "/tg/webhook";
 }
